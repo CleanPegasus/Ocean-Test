bffb29efcc729ff0884d9ebe2e4b6b7d99206022
Arunkumar L <arunk609@gmail.com>
2021-07-27 14:52:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -166,8 +166,8 @@ if __name__ == '__main__':
     job, did = get_job_details()
     X, Y = preprocess_data(job)
 
-    did_mapping = {'0': 'decision_tree', 
-                   '1': 'mlp_classifier', 
+    did_mapping = {'07A7287F45471dA8d7BddC647d49f03a54672E38': 'decision_tree', 
+                   '2907E1f782f59C7B515c80B2DDB9DaC388F377F5': 'mlp_classifier', 
                    '2': 'naive_bayes'}
 
     algo = did_mapping[did]
