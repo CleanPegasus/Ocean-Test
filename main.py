32d272f0c8d7e5f53f85a64668db04af82552207
Arunkumar L <arunk609@gmail.com>
2021-07-29 12:04:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,57 @@ import os
 import time
 import json
 import pandas as pd
+from sklearn import metrics
+from sklearn.metrics import classification_report,accuracy_score,confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler,LabelEncoder,scale
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import svm
+from sklearn.svm import SVR
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import pickle
 
 encode = False
 y_le = LabelEncoder() 
+
+def get_job_details():
+    """Reads in metadata information about assets used by the algo"""
+    job = dict()
+    job['dids'] = json.loads(os.getenv('DIDS', None))
+    job['metadata'] = dict()
+    job['files'] = dict()
+    job['algo'] = dict()
+    job['secret'] = os.getenv('secret', None)
+    algo_did = os.getenv('TRANSFORMATION_DID', None)
+    if job['dids'] is not None:
+        for did in job['dids']:
+            # get the ddo from disk
+            filename = '/data/ddos/' + did
+            print(f'Reading json from {filename}')
+            with open(filename) as json_file:
+                ddo = json.load(json_file)
+                # search for metadata service
+                for service in ddo['service']:
+                    if service['type'] == 'metadata':
+                        job['files'][did] = list()
+                        index = 0
+                        for file in service['attributes']['main']['files']:
+                            job['files'][did].append(
+                                '/data/inputs/' + did + '/' + str(index))
+                            index = index + 1
+    if algo_did is not None:
+        job['algo']['did'] = algo_did
+        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+    return job
 
 def preprocess_data(job_details):
     '''
@@ -18,6 +66,7 @@ def preprocess_data(job_details):
 
     first_did = job_details['dids'][0]
     filename = job_details['files'][first_did][0]
+    # filename = job_details # remove this later
     global encode
     global y_le
 
@@ -44,7 +93,7 @@ def preprocess_data(job_details):
     return (X_train, X_test, Y_train, Y_test)
 
 class Supervised:
-    def __init__(self,X_train,X_test,Y_train,Y_test):
+    def __init__(self, algo, X_train,X_test,Y_train,Y_test):
         sc = StandardScaler()
         self.X_train = sc.fit_transform(X_train)
         self.X_test = sc.transform(X_test)
@@ -56,41 +105,20 @@ class Supervised:
         self.confusion_matrix = None
         self.classification_report = None
 
-    def decision_tree(self):
-        self.model = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+        if(algo == 'decision_tree'):
+            self.model = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 
-    def mlp_classifier(self):
-        self.model = MLPClassifier(hidden_layer_sizes=(50), activation='tanh', alpha =0.009, max_iter=250 )
+        elif(algo == 'mlp_classifier'):
+            self.model = MLPClassifier(hidden_layer_sizes=(50), activation='tanh', alpha =0.009, max_iter=250 )
 
-    def naive_bayes(self):
-        self.model = GaussianNB()
+        elif(algo == 'naive_bayes'):
+            self.model = GaussianNB()
 
-    def linear_regression(self):
-        self.model = LinearRegression()
+        elif(algo == 'linear_regression'):
+            self.model = LinearRegression()
 
-    def logistic_regression(self):
-        self.model = LogisticRegression(C=1e5)
-    
-    def MLP_Regressor(self):
-        self.model = MLPRegressor(random_state = 1, max_iter = 500)
-
-    def svm_classification(self):
-        self.model = svm.SVC(kernel='linear')
-
-    def random_forest_regressor(self):
-        self.model = RandomForestRegressor(n_estimators=100, max_depth=2)
-
-    def svr(self):
-        self.model = SVR(kernel='linear')
-
-    def elasticnet(self):
-        self.model = ElasticNet(alpha=0.01)
-
-    def non_lin_svm(self):
-        self.model = svm.NuSVC(gamma = 'auto')
-    
-    def CART(self):
-        self.model= DecisionTreeClassifier(random_state = 100)
+        elif(algo == 'logistic_regression'):
+            self.model = LogisticRegression(C=1e5)
     
     def train(self):
         self.model.fit(self.X_train, self.Y_train)
@@ -102,76 +130,73 @@ class Supervised:
         self.classification_report = classification_report(self.Y_test, self.predictions)
         result = str(self.predictions) + '\n' + str(self.accuracy) + '\n' + str(self.confusion_matrix) + '\n' + str(self.classification_report)
         return result
-         
-x,y, = preprocess_data()
-sup = Supervised(X_train,X_test,Y_train,Y_test)
-algo = input('Enter the algorithm to be used: ')
-build_model(algo)
 
+class Unsupervised:
+    def __init__(self, algo, X_train,X_test,Y_train,Y_test):
+        self.model = None
+        self.predictions = None
+        self.X_train = X_train
+        self.X_test = X_test
+        self.Y_train = Y_train
+        self.Y_test = Y_test
 
-def build_model(algo):
-    if(algo == 'decision_tree'):
+        if(algo == 'KMeans'):
+            self.model = KMeans(n_clusters=2, random_state=0)
         
-
+        elif(algo == 'Gaussian_Mixture'):
+            self.model = GaussianMixture(n_components=2, random_state=0)
         
-    elif(algo == 'mlp_classifier'):
-        mlp_classifier(X,Y)
-    elif(algo == 'naive_bayes'):
-        naive_bayes(X, Y)
-    elif(algo == 'k_means'):
-        k_means(X)
-    elif(algo == 'knn'):
-        knn(X, Y)
-    elif(algo == 'linear_regression'):
-        linear_regression(X, Y)
-    elif(algo == 'logistic_regression'):
-        logistic_regression(X, Y)
-    elif(algo == 'MLP_Regressor'):
-        MLP_Regressor(X, Y)
-    elif(algo == 'svm_classification'):
-        svm_classification(X, Y)
-    elif(algo == 'random_forest_regressor'):
-        random_forest_regressor(X, Y)
-    elif(algo == 'svr'):
-        svr(X, Y)
-    else:
-        print('Invalid DID')
+        elif(algo == 'DBSCAN'):
+            self.model = DBSCAN(eps=0.1)
 
-def train_model():
-    pass
+    def train(self):
+        self.model.fit(self.X_train)
 
-def test_model():
-    pass
+    def predict(self):
+        self.predictions = self.model.predict(self.X_test)
+        return self.predictions
 
-def get_results():
-    pass
+
 
 if __name__ == '__main__':
 
     job = get_job_details()
     did = job['algo']['did']
-    X, Y = preprocess_data(job)
 
-    did_mapping = {'07A7287F45471dA8d7BddC647d49f03a54672E38': 'decision_tree', 
-                   '2907E1f782f59C7B515c80B2DDB9DaC388F377F5': 'mlp_classifier', 
-                   '2331e8116b0acB7AC164d8B4F332Aa104Eb9790F': 'naive_bayes',
+    # job = 'iris.csv'
+    # did = '2907E1f782f59C7B515c80B2DDB9DaC388F377F5'
+
+    did_mapping = {'07A7287F45471dA8d7BddC647d49f03a54672E38': 'supervised/decision_tree', 
+                   '2907E1f782f59C7B515c80B2DDB9DaC388F377F5': 'supervised/mlp_classifier', 
+                   '2331e8116b0acB7AC164d8B4F332Aa104Eb9790F': 'supervised/naive_bayes',
                    '52D80495e56CFB241fD9e06aF7b5B96a80Ba509F': 'k_means',
-                   '47838B1A397ed620F51D079Cd456d6564f940aC8': 'knn',
-                   '3476E489Fb00058298fC8959Cb535fD3C29612c2': 'linear_regression',
-                   '50374cfC875D9a66Be3f795Ff52Cb7714819eb7A': 'logistic_regression',
-                   'e90F3344D508d017564b8EB4BB7e2E7C858365aa': 'MLP_Regressor',
-                   '03115a5Dc5fC8Ff8DA0270E61F87EEB3ed2b3798': 'svm_classification',
-                   '87F1A31A008D9cBE5c49B06dDb608df56967Cd51': 'random_forest_regressor',
-                   '60b71c78E17de953A84f3A5A876645Cf15c1A92b': 'svr',
-                   '0': 'elasticnet',
-                   '1': 't_sne',
-                   '2': 'non_lin_svm',
-                   '3': 'PCA',
-                   '4': 'CART'            
+                   '47838B1A397ed620F51D079Cd456d6564f940aC8': 'unsupervised/knn',
+                   '3476E489Fb00058298fC8959Cb535fD3C29612c2': 'supervised/linear_regression',
+                   '50374cfC875D9a66Be3f795Ff52Cb7714819eb7A': 'supervised/logistic_regression',
+                   'e90F3344D508d017564b8EB4BB7e2E7C858365aa': 'supervised/MLP_Regressor',
+                   '03115a5Dc5fC8Ff8DA0270E61F87EEB3ed2b3798': 'supervised/svm_classification',
+                   '87F1A31A008D9cBE5c49B06dDb608df56967Cd51': 'supervised/random_forest_regressor',
+                   '60b71c78E17de953A84f3A5A876645Cf15c1A92b': 'supervised/svr',
+                   '0': 'supervised/elasticnet',
+                   '1': 'unsupervised/t_sne',
+                   '2': 'supervised/non_lin_svm',
+                   '3': 'unsupervised/PCA',
+                   '4': 'supervised/CART'            
                    }
 
-    algo = did_mapping[did]
-    build_model(algo)
-    train_model()
-    test_model()
-    get_results()
+    algo_type = did_mapping[did].split('/')[0]
+    algo = did_mapping[did].split('/')[1]
+
+    X_train,X_test,Y_train,Y_test = preprocess_data(job)
+
+
+    if algo_type == 'unsupervised':
+        unsupervised = Unsupervised(algo, X_train, Y_train, X_test, Y_test)
+        unsupervised.train()
+        result =unsupervised.predict()
+        print(result)
+    elif algo_type == 'supervised':
+        supervised = Supervised(algo, X_train,X_test,Y_train,Y_test)
+        supervised.train()
+        result = supervised.predict()
+        print(result)
